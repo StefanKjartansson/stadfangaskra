8828c979a11fc8597edf8b041d0fa699175d05f7
Stefan Kjartansson <esteban.supreme@gmail.com>
2016-02-10 05:45:35 +0000

    Add docs, golint

--- bydistance.go
+++ bydistance.go
@@ -2,7 +2,7 @@ package stadfangaskra
 
 import "math"
 
-const R = 6371
+const r = 6371
 
 func distanceInKm(p1, p2 Point) float64 {
 	a := 0.5 -
@@ -11,9 +11,10 @@ func distanceInKm(p1, p2 Point) float64 {
 		math.Cos(p1.X*math.Pi/180)*
 			math.Cos(p2.X*math.Pi/180)*
 			(1-math.Cos((p2.Y-p1.Y)*math.Pi/180))/2
-	return R * 2 * math.Asin(math.Sqrt(a))
+	return r * 2 * math.Asin(math.Sqrt(a))
 }
 
+// ByDistance returns a FindFilter matching addresses within the given radius in km.
 func ByDistance(p Point, distance float64) (FindFilter, error) {
 	return func(l *Location) bool {
 		return distanceInKm(p, l.Coordinates) <= distance
